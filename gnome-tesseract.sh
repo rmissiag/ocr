725aa465307f838abac412b8c4b5fb51dd6508c7
Rodrigo Missiaggia <rmissiaggia@gmail.com>
2024-10-15 20:34:56 -0300

    Update gnome-tesseract.sh

--- gnome-tesseract.sh
+++ gnome-tesseract.sh
@@ -7,7 +7,8 @@ set -euo pipefail
 # Caminho temporário para salvar a captura de tela
 TEMP_IMAGE=`mktemp /tmp/gnome_tesseract_XXXXXX.png`
 TEMP_TEXT=`mktemp /tmp/gnome_tesseract_XXXXXX.txt`
-# Função para limpar o arquivo temporário ao sair do script
+
+# Função para limpar o arquivo temporário ao encerrar o script
 cleanup() {
     rm -f "${TEMP_IMAGE}" "${TEMP_TEXT}"
 }
@@ -18,13 +19,14 @@ gnome-screenshot -a -f "${TEMP_IMAGE}"
 
 # Verificar se a captura foi bem-sucedida
 if [ -f "${TEMP_IMAGE}" ]; then
-    # Copiar a captura para a área de transferência
+    # Executar o tesseract com detecção de texto em português (reconhece também inglês).
     tesseract -l por "${TEMP_IMAGE}" stdout > "${TEMP_TEXT}"
 
-    # Abrir o navegador com a página do Google Lens
+    # Abrir o navegador com o txt gerado
     "${BROWSER}" --new-tab file:///${TEMP_TEXT}
     sleep 3
 else
     echo "Erro: Falha ao capturar a tela." >&2
     exit 1
 fi
+
